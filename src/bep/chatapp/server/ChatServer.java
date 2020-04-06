d9eed997bc93a78ece05183a8359932b7032fea7
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2020-04-06 14:10:51 +0200

    Added ChatAppController with comments Update ChatServer (to single ServerSocket)

--- src/bep/chatapp/server/ChatServer.java
+++ src/bep/chatapp/server/ChatServer.java
@@ -5,32 +5,48 @@ import java.net.*;
 import java.util.*;
 
 public class ChatServer {
+    final static int port = 8080;
+
     public static void main(String[] args) throws IOException {
         List<PrintWriter> allClients = new ArrayList<PrintWriter>();
-        while (true) {
-            try (ServerSocket ss = new ServerSocket(8080)) {
-                Socket s = ss.accept();
 
-                new Thread() {
-                    public void run() {
-                        PrintWriter pw = null;
-                        try (Scanner scanner = new Scanner(s.getInputStream())) {
-                            allClients.add(pw = new PrintWriter(s.getOutputStream()));
+        try (ServerSocket ss = new ServerSocket(port)) {
+            System.out.printf("Server started on port %d!\n", port);
 
-                            while (scanner.hasNextLine()) {
-                                String message = scanner.nextLine();
-                                for (PrintWriter printer : allClients) {
-                                    printer.println(message);
-                                    printer.flush();
+            while (true) {
+                try {
+                    Socket s = ss.accept();
+
+                    // voor elke nieuwe client een aparte thread om binnenkomende chatberichten te verwerken
+                    new Thread() {
+                        public void run() {
+                            PrintWriter pw = null;
+
+                            // try-with-resource (scanner wordt na afloop automatisch gesloten):
+                            try (Scanner scanner = new Scanner(s.getInputStream())) {
+                                allClients.add(pw = new PrintWriter(s.getOutputStream()));
+
+                                while (scanner.hasNextLine()) {
+                                    String message = scanner.nextLine();
+                                    System.out.println("Incoming and distributing: " + message);
+
+                                    // schrijf het binnenkomende bericht naar alle clients!
+                                    for (PrintWriter printer : allClients) {
+                                        printer.println(message);
+                                        printer.flush();
+                                    }
                                 }
+                            } catch (IOException ioe) {
+                                ioe.printStackTrace();
+                            } finally {
+                                System.out.println("Client-verbinding verbroken!");
+                                allClients.remove(pw);
                             }
-                        } catch (IOException ioe) {
-                            ioe.printStackTrace();
-                        } finally {
-                            allClients.remove(pw);
                         }
-                    }
-                }.start();
+                    }.start();
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
             }
         }
     }
